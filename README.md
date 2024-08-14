390743fdd8c5ea6e27d579b9a405cf2889193ce9
satyajitPatel <thriver1988@gmail.com>
2024-08-15 03:46:33 +0530

    v2 add copy to clip-board method

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 
 ## concepts
-- Hooks: useState() | useCallback() | useEffect()
+- Hooks and Even-handler: 
+```
+    useState()      - variable
+    useCallback()   - memoization 
+    useEffect()     - run if any changes happen
+    useRef()        - get the iterator
+
+    onClick()       - handle's the event
+
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect} from 'react'
+import { useState, useCallback, useEffect, useRef} from 'react'
 
 function App() {
   let [length, setLength] = useState(8);
@@ -28,13 +28,41 @@ function App() {
   // this is for run
   useEffect(() => {getPassword()}, [length, isContainNumber, isContainSpecialCharacter, getPassword]);
 
+  // useRef Hook
+  const passwordRef = useRef(null);
+
+  // we can optimize this also
+  const copyPassToClipBoard = useCallback(() => {
+    // we can know a lot thing about passwor through password reference
+    // passwordRef
+    passwordRef.current?.select(); // shows blue background sign that pass has been selected
+    // ? means optionally select as value could be zero
+    passwordRef.current?.setSelectionRange(0, 100);
+    window.navigator.clipboard.writeText(password);
+  }, [password]); // I think in dependencies only passwod will be affect
+
+  const [buttonText, setButtonText] = useState('Copy');
+  const handleClick = () => {
+      setButtonText('Copied!');
+      // Reset the button text back to 'Copy' after a 2-second delay
+      setTimeout(() => {
+          setButtonText('Copy');
+      }, 2000);
+  };
+
   return (
     <>
-        <div className='h-screen w-screen bg-one flex flex-wrap flex-col justify-center items-center gap-7'>
+        <div className='h-screen w-screen bg-one flex flex-wrap flex-col justify-center items-center gap-3'>
             <h1 className='h-max w-max bg-two hover:bg-three rounded-lg text-4xl'>Password Generator</h1>
-            <div className='h-max w-max bg-four hover:bg-five rounded-lg'>
-                <input type="text" value={password} placeholder='password' readOnly className='rounded-lg p-2 m-2 bg-one' />
-                <button className='h-10 w-10 rounded-lg bg-blue-700'>Copy</button>
+            <div className='h-max min-w-9 bg-four hover:bg-five rounded-lg'>
+                <input type="text" value={password} placeholder='password' readOnly ref={passwordRef} className='rounded-lg p-2 m-2 bg-one w-80' />
+                {/* <button className='h-10 w-10 rounded-lg bg-blue-700' onClick={copyPassToClipBoard}>Copy</button> */}
+                <button
+                  onClick={() => {
+                    copyPassToClipBoard();
+                    handleClick(); 
+                  }} 
+                  className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">{buttonText}</button>
             </div>
             <div>
                 <div>
